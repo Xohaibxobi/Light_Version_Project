526686a231ea61b22aa8b650b7d3be72c588403c
Xohaibxobi <hafizzohaibamin@gmail.com>
2023-05-09 20:10:40 +0500

    5/9/2023

---
+++ app/src/main/java/com/example/lightversionproject/LightLestYouIn.kt
@@ -0,0 +1,11 @@
+package com.example.lightversionproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LightLestYouIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_light_lest_you_in)
+    }
+}
